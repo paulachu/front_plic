b415e96ddd74145eaa89d670484207fece953cb4
Paul Radic <paul.radic@epita.fr>
2021-09-26 17:21:27 +0200

    [LIGHT][PAGE] add card desgin and logic for the light page

---
+++ src/app/components/light-card/light-card.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Input } from '@angular/core';
+import {LightInterface} from "../../interfaces/light-interface";
+
+@Component({
+  selector: 'app-light-card',
+  templateUrl: './light-card.component.html',
+  styleUrls: ['./light-card.component.scss']
+})
+export class LightCardComponent implements OnInit {
+
+  @Input()
+  light!: LightInterface;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  getColor(): string
+  {
+    return 'rgba(' + this.light.r + ',' + this.light.g + ',' + this.light.b + ',' + this.light.a + ')';
+  }
+}
